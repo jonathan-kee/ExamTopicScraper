46a68ef152541ea72d4bc0b82d2b4a012587ed5b
jonathan kee <jonathankee99@gmail.com>
2026-08-01 13:47:32 +0800

    update sql

--- sql/setupDBT.sql
+++ sql/setupDBT.sql
@@ -74,3 +74,8 @@ CREATE TABLE scrape.discussions
 CREATE SEQUENCE scrape.seq_questions START WITH 1 INCREMENT BY 1;
 SELECT last_value FROM scrape.seq_questions;
 SELECT nextval('scrape.seq_questions') as next_value;
+
+
+CREATE SEQUENCE scrape.seq_imagesLink START WITH 1 INCREMENT BY 1;
+SELECT last_value FROM scrape.seq_imagesLink;
+SELECT nextval('scrape.seq_imagesLink') as next_value;
